640c66167fd7ed244c0d776de253b0699bf4f7da
shorookMS <shorook.m.s@gmail.com>
2018-10-07 15:37:23 +0300

    task submission

--- restaurants/views.py
+++ restaurants/views.py
@@ -4,16 +4,34 @@ def welcome(request):
     return render(request, 'index.html', {'msg':'Hello World!'})
 
 def restaurant_list(request):
+	restlist= [
+	{
+		"restaurant_name" :"Sunny Sideup",
+		"food_type" : "Breakfast"
+	},
+	{
+		"restaurant_name" :"Dead Land",
+		"food_type" : "Brains for Zombies"
+	},
+	{
+		"restaurant_name" :"Suger Rush",
+		"food_type" : "Candies and Cakes"
+	}
 
-    context = {
-
-    }
-    return render(request, 'list.html', context)
+	]
+	context = {
+	"my_list" : restlist
+	}
+	return render(request, 'list.html', context)
 
 
 def restaurant_detail(request):
+	dictionary ={
+		"restaurant_name" :"Dead Land",
+		"food_type" : "Brains for Zombies"
 
-    context = {
-
-    }
-    return render(request, 'detail.html', context)
+	}
+	context = {
+	"my_object" : dictionary
+	}
+	return render(request, 'detail.html', context)
